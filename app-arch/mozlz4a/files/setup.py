c782e466328a30b200b51e5cbf3bd8e8e559a6d3
Paul Goller <hmp@h4emp3.net>
2016-11-17 08:57:18 +0100

    app-arch/mozlz4a: add new ebuild with version from github gist
    Package-Manager: portage-2.3.0

---
+++ app-arch/mozlz4a/files/setup.py
@@ -0,0 +1,41 @@
+"""
+setuptools configuration for mozlz4a
+
+"""
+
+
+import os
+import sys
+import setuptools
+
+
+NAME = 'mozlz4a'
+DESCRIPTION = 'MozLz4a compression/decompression utility.'
+AUTHOR = 'Tilman Blumenbach'
+URL = 'https://gist.github.com/Tblue/62ff47bef7f894e92ed5'
+LICENSE = 'GPLv2'
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Topic :: System :: Archiving :: Compression',
+]
+
+
+data = dict(
+    name=NAME,
+    description=DESCRIPTION,
+    url=URL,
+    author=AUTHOR,
+    license=LICENSE,
+    packages=['mozlz4a'],
+    entry_points={'console_scripts': ['mozlz4a=mozlz4a:__main__']},
+    install_requires=['lz4'],
+    classifiers=CLASSIFIERS,
+)
+
+if __name__ == '__main__':
+    setuptools.setup(**data)
---
+++ app-arch/mozlz4a/mozlz4a-20150724.ebuild
@@ -0,0 +1,29 @@
+# Copyright 1999-2016 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Id$
+
+EAPI=5
+PYTHON_COMPAT=( python3_4 )
+
+inherit distutils-r1 git-r3
+
+DESCRIPTION="MozLz4a compression/decompression utility."
+HOMEPAGE="https://gist.github.com/Tblue/62ff47bef7f894e92ed5"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="~amd64"
+IUSE=""
+
+DEPEND="dev-python/lz4"
+RDEPEND="${DEPEND}"
+
+EGIT_REPO_URI="https://gist.github.com/62ff47bef7f894e92ed5.git"
+EGIT_COMMIT="2483756c55ed34be565aea269f05bd5eeb6b0a33"
+
+src_prepare() {
+	mkdir "${WORKDIR}/${P}/mozlz4a"
+	touch "${WORKDIR}/${P}/mozlz4a"
+	mv "${WORKDIR}/${P}/mozlz4a.py" "${WORKDIR}/${P}/mozlz4a/__main__.py"
+	cp "${FILESDIR}/setup.py" "${WORKDIR}/${P}/"
+}
